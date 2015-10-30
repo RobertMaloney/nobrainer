278f37fa0a4234d32bcc19b0b9d16b7eeb2d81a7
Robert Maloney <robert.maloney@mycase.com>
2015-10-29 17:14:00 -0700

    rm: use program counter

--- clojure/main.clj
+++ clojure/main.clj
@@ -2,9 +2,10 @@
   {:tape (apply vector-of :byte (repeat 30000 0))
    :data-pointer 0})
 
-(defn runcommand [command state]
-  (let [dp (:data-pointer state)]
-    (case command
+(defn runcommand [program state pc]
+  (let [dp (:data-pointer state)
+        command (get program pc)]
+    [(case command
       \+ (update-in state [:tape dp] inc)
       \- (update-in state [:tape dp] dec)
       \> (update-in state [:data-pointer] inc)
@@ -12,10 +13,14 @@
       \. (do
             (print (char (nth (:tape state) dp)))
             state)
-      state)))
+      state)
+    (case command
+      pc)]))
 
 (defn interpret [filename]
-  (loop [in (slurp filename)
-         state initial-state]
-    (if (> (count in) 0)
-      (recur (rest in) (runcommand (first in) state)))))
+    (loop [in (slurp filename)
+           state initial-state
+           pc 0]
+      (if (< pc (count in))
+        (let [[newstate newpc] (runcommand in state pc)]
+          (recur in newstate (+ newpc 1))))))
