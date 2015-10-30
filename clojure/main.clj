70efec8f685b871db7bee56e479873daa34adaf2
Robert Maloney <robert.maloney@mycase.com>
2015-10-29 17:38:09 -0700

    rm: implement brackets in clojure

--- clojure/main.clj
+++ clojure/main.clj
@@ -2,6 +2,30 @@
   {:tape (apply vector-of :byte (repeat 30000 0))
    :data-pointer 0})
 
+(defn goback [program state pc]
+  (if (not= 0 (get (:tape state) (:data-pointer state)))
+    (loop [counter 1
+           pointer (dec pc)]
+      (if (> counter 0)
+        (recur (case (get program pointer)
+          \[ (dec counter)
+          \] (inc counter)
+          counter) (dec pointer))
+        pointer))
+    pc))
+
+(defn goforward [program state pc]
+  (if (= 0 (get (:tape state) (:data-pointer state)))
+    (loop [counter 1
+           pointer (inc pc)]
+      (if (> counter 0)
+        (recur (case (get program pointer)
+          \[ (inc counter)
+          \] (dec counter)
+          counter) (inc pointer))
+        pointer))
+    pc))
+
 (defn runcommand [program state pc]
   (let [dp (:data-pointer state)
         command (get program pc)]
@@ -15,6 +39,8 @@
             state)
       state)
     (case command
+      \[ (goforward program state pc)
+      \] (goback program state pc)
       pc)]))
 
 (defn interpret [filename]
