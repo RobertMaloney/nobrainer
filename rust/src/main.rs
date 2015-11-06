21682e842a6a2699f42f42c7ff810755c1d58153
Robert Maloney <robert.maloney@mycase.com>
2015-11-06 14:46:41 -0800

    rm: fix indentation for rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,28 +6,28 @@ mod brain;
 use brain::Brain;
 
 fn grab_file_contents(path: &str) -> String {
-    let mut f = File::open(path).unwrap();
-    let mut s = String::new();
-    f.read_to_string(&mut s).unwrap();
+  let mut f = File::open(path).unwrap();
+  let mut s = String::new();
+  f.read_to_string(&mut s).unwrap();
 
-    return s;
+  return s;
 }
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() < 3 {
-        println!("Not enough arguments!");
-        return;
-    }
+  let args: Vec<_> = env::args().collect();
+  if args.len() < 3 {
+    println!("Not enough arguments!");
+    return;
+  }
 
-    match args[1].as_ref() {
-        "generate" => {
-            Brain::generate(&args[2]);
-        },
-        "interpret" => {
-            let program = grab_file_contents(&args[2]);
-            Brain::interpret(&program);
-        },
-        _ => println!("Operation not supported.")
-    }
+  match args[1].as_ref() {
+    "generate" => {
+      Brain::generate(&args[2]);
+    },
+    "interpret" => {
+      let program = grab_file_contents(&args[2]);
+      Brain::interpret(&program);
+    },
+    _ => println!("Operation not supported.")
+  }
 }
